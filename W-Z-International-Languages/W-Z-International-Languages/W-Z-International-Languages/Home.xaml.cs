2edb11762173a8b63149fe6658fcbc8eac6fc216
wael zobani <waelzobani93@gmail.com>
2022-11-13 03:47:26 +0100

    finish course style

--- W-Z-International-Languages/W-Z-International-Languages/W-Z-International-Languages/Home.xaml.cs
+++ W-Z-International-Languages/W-Z-International-Languages/W-Z-International-Languages/Home.xaml.cs
@@ -32,64 +32,102 @@ namespace W_Z_International_Languages
             this.Close();
         }
 
+        private void Dashboard_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Student_Mang.Visibility = Visibility.Hidden;
+            Student_Registration.Visibility = Visibility.Hidden;
+            New_Course.Visibility = Visibility.Hidden;
+            Mang_Course.Visibility = Visibility.Hidden;
+            //View_Student.Visibility = Visibility.Hidden;
+
+        }
+        //private void MenuItem_view_Student_Click(object sender, RoutedEventArgs e)
+        //{
+        //    View_Student.Visibility = Visibility.Hidden;
+        //    Student_Registration.Visibility = Visibility.Hidden;
+        //    Student_Mang.Visibility = Visibility.Hidden;
+        //    New_Course.Visibility = Visibility.Hidden;
+        //    Mang_Course.Visibility = Visibility.Hidden;
+        //}
         private void MenuItem_Registration_Click(object sender, RoutedEventArgs e)
         {
             Student_Registration.Visibility = Visibility.Visible;
-            Student_Mang1.Visibility = Visibility.Hidden;
+            Student_Mang.Visibility = Visibility.Hidden;
             New_Course.Visibility = Visibility.Hidden;
+            Mang_Course.Visibility = Visibility.Hidden;
+            //View_Student.Visibility = Visibility.Hidden;
 
         }
 
         private void MenuItem_Manage_Student_Click(object sender, RoutedEventArgs e)
         {
-            Student_Mang1.Visibility = Visibility.Visible;
+            Student_Mang.Visibility = Visibility.Visible;
             Student_Registration.Visibility = Visibility.Hidden;
             New_Course.Visibility = Visibility.Hidden;
+            Mang_Course.Visibility = Visibility.Hidden;
+            //View_Student.Visibility = Visibility.Hidden;
 
         }
 
-        private void Dashboard_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            Student_Mang1.Visibility = Visibility.Hidden;
-            Student_Registration.Visibility = Visibility.Hidden;
-            New_Course.Visibility = Visibility.Hidden;
-
-        }
-
-        private void Course_Click(object sender, RoutedEventArgs e)
+        private void New_Course_Click(object sender, RoutedEventArgs e)
         {
             New_Course.Visibility = Visibility.Visible;
-            Student_Mang1.Visibility = Visibility.Hidden;
+            Student_Mang.Visibility = Visibility.Hidden;
             Student_Registration.Visibility = Visibility.Hidden;
+            Mang_Course.Visibility = Visibility.Hidden;
+            //View_Student.Visibility = Visibility.Hidden;
+
         }
 
-        private void tbx_search_GotFocus(object sender, RoutedEventArgs e)
+        private void Mang_Course_Click(object sender, RoutedEventArgs e)
         {
-            if (tbx_search.Text == "search")
+            Mang_Course.Visibility = Visibility.Visible;
+            New_Course.Visibility = Visibility.Hidden;
+            Student_Mang.Visibility = Visibility.Hidden;
+            Student_Registration.Visibility = Visibility.Hidden;
+            //View_Student.Visibility = Visibility.Hidden;
+
+        }
+
+        //private void tbx_View_Student_search_GotFocus(object sender, RoutedEventArgs e)
+        //{
+        //    if (tbx_View_Student_search.Text == "Search")
+        //    {
+        //        tbx_View_Student_search.Text = "";
+        //    }
+        //}
+        private void tbx_Student_Registration_search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbx_Student_Registration_search.Text == "Search")
             {
-                tbx_search.Text = "";
+                tbx_Student_Registration_search.Text = "";
             }
         }
 
-        private void tbx_Search_GotFocus_1(object sender, RoutedEventArgs e)
+        private void tbx_Student_Mang_Search_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (tbx_Search.Text == "search")
+            if (tbx_Student_Mang_Search.Text == "Search")
             {
-                tbx_Search.Text = "";
+                tbx_Student_Mang_Search.Text = "";
+            }
+        }
+        
+        private void tbx_New_Course_Search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbx_New_Course_Search.Text == "Search")
+            {
+                tbx_New_Course_Search.Text = "";
             }
         }
 
-        private void tbxs_Search_GotFocus(object sender, RoutedEventArgs e)
+        private void tbx_Mang_Course_Search_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (tbx_Search.Text == "search")
+            if (tbx_Mang_Course_Search.Text == "Search")
             {
-                tbx_Search.Text = "";
+                tbx_Mang_Course_Search.Text = "";
             }
-
         }
 
-     
-
-     
+    
     }
 }
