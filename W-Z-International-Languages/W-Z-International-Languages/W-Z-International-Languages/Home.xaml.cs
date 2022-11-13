307f30518502b916167b870de1d0d6167309e96b
wael zobani <waelzobani93@gmail.com>
2022-11-13 17:18:33 +0100

    Create Print function

--- W-Z-International-Languages/W-Z-International-Languages/W-Z-International-Languages/Home.xaml.cs
+++ W-Z-International-Languages/W-Z-International-Languages/W-Z-International-Languages/Home.xaml.cs
@@ -38,24 +38,29 @@ namespace W_Z_International_Languages
             Student_Registration.Visibility = Visibility.Hidden;
             New_Course.Visibility = Visibility.Hidden;
             Mang_Course.Visibility = Visibility.Hidden;
-            //View_Student.Visibility = Visibility.Hidden;
+            View_Student.Visibility = Visibility.Hidden;
+            View_Course.Visibility = Visibility.Hidden;
 
         }
-        //private void MenuItem_view_Student_Click(object sender, RoutedEventArgs e)
-        //{
-        //    View_Student.Visibility = Visibility.Hidden;
-        //    Student_Registration.Visibility = Visibility.Hidden;
-        //    Student_Mang.Visibility = Visibility.Hidden;
-        //    New_Course.Visibility = Visibility.Hidden;
-        //    Mang_Course.Visibility = Visibility.Hidden;
-        //}
+        private void MenuItem_view_Student_Click(object sender, RoutedEventArgs e)
+        {
+            View_Student.Visibility = Visibility.Visible;
+            Student_Registration.Visibility = Visibility.Hidden;
+            Student_Mang.Visibility = Visibility.Hidden;
+            New_Course.Visibility = Visibility.Hidden;
+            Mang_Course.Visibility = Visibility.Hidden;
+            View_Course.Visibility = Visibility.Hidden;
+
+
+        }
         private void MenuItem_Registration_Click(object sender, RoutedEventArgs e)
         {
             Student_Registration.Visibility = Visibility.Visible;
             Student_Mang.Visibility = Visibility.Hidden;
             New_Course.Visibility = Visibility.Hidden;
             Mang_Course.Visibility = Visibility.Hidden;
-            //View_Student.Visibility = Visibility.Hidden;
+            View_Student.Visibility = Visibility.Hidden;
+            View_Course.Visibility = Visibility.Hidden;
 
         }
 
@@ -65,17 +70,27 @@ namespace W_Z_International_Languages
             Student_Registration.Visibility = Visibility.Hidden;
             New_Course.Visibility = Visibility.Hidden;
             Mang_Course.Visibility = Visibility.Hidden;
-            //View_Student.Visibility = Visibility.Hidden;
+            View_Student.Visibility = Visibility.Hidden;
+            View_Course.Visibility = Visibility.Hidden;
 
         }
-
+        private void View_Course_Click(object sender, RoutedEventArgs e)
+        {
+            View_Course.Visibility = Visibility.Visible;
+            Student_Mang.Visibility = Visibility.Hidden;
+            Student_Registration.Visibility = Visibility.Hidden;
+            New_Course.Visibility = Visibility.Hidden;
+            Mang_Course.Visibility = Visibility.Hidden;
+            View_Student.Visibility = Visibility.Hidden;
+        }
         private void New_Course_Click(object sender, RoutedEventArgs e)
         {
             New_Course.Visibility = Visibility.Visible;
             Student_Mang.Visibility = Visibility.Hidden;
             Student_Registration.Visibility = Visibility.Hidden;
             Mang_Course.Visibility = Visibility.Hidden;
-            //View_Student.Visibility = Visibility.Hidden;
+            View_Student.Visibility = Visibility.Hidden;
+            View_Course.Visibility = Visibility.Hidden;
 
         }
 
@@ -85,17 +100,18 @@ namespace W_Z_International_Languages
             New_Course.Visibility = Visibility.Hidden;
             Student_Mang.Visibility = Visibility.Hidden;
             Student_Registration.Visibility = Visibility.Hidden;
-            //View_Student.Visibility = Visibility.Hidden;
-
+            View_Student.Visibility = Visibility.Hidden;
+            View_Course.Visibility = Visibility.Hidden;
         }
 
-        //private void tbx_View_Student_search_GotFocus(object sender, RoutedEventArgs e)
-        //{
-        //    if (tbx_View_Student_search.Text == "Search")
-        //    {
-        //        tbx_View_Student_search.Text = "";
-        //    }
-        //}
+        private void tbx_View_Student_search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbx_View_Student_search.Text == "Search")
+            {
+                tbx_View_Student_search.Text = "";
+            }
+        }
+   
         private void tbx_Student_Registration_search_GotFocus(object sender, RoutedEventArgs e)
         {
             if (tbx_Student_Registration_search.Text == "Search")
@@ -111,7 +127,29 @@ namespace W_Z_International_Languages
                 tbx_Student_Mang_Search.Text = "";
             }
         }
-        
+        private void MenuItem_Student_Print_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog dlg = new PrintDialog();
+
+            Window currentMainWindow = Application.Current.MainWindow;
+
+            Application.Current.MainWindow = this;
+
+            if ((bool)dlg.ShowDialog().GetValueOrDefault())
+            {
+                Application.Current.MainWindow = currentMainWindow;
+                dlg.PrintVisual(DG_Student, "Auftrag_Nr");
+
+            }
+
+        }
+        private void tbx_view_Course_Search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbx_view_Course_Search.Text == "Search")
+            {
+                tbx_view_Course_Search.Text = "";
+            }
+        }
         private void tbx_New_Course_Search_GotFocus(object sender, RoutedEventArgs e)
         {
             if (tbx_New_Course_Search.Text == "Search")
@@ -128,6 +166,24 @@ namespace W_Z_International_Languages
             }
         }
 
-    
+       
+
+        private void MenuItem_Course_Print_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog dlg = new PrintDialog();
+
+            Window currentMainWindow = Application.Current.MainWindow;
+
+            Application.Current.MainWindow = this;
+
+            if (dlg.ShowDialog().GetValueOrDefault())
+            {
+                Application.Current.MainWindow = currentMainWindow;
+                dlg.PrintVisual(DG_View_Course, "Auftrag_Nr");
+
+            }
+        }
+
+   
     }
 }
